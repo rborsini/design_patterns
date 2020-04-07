609d85068c04af0920e0c9d77c1f6693392fd2ac
Roberto Borsini <roberto.borsini@gmail.com>
2020-04-07 22:50:12 +0200

    Abstract factory

---
+++ FactoryMethod/AbstractFactory/ConcreteProductA2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace AbstractFactory
+{
+    class ConcreteProductA2 : IAbstractProductA
+    {
+        public string UsefulFunctionA()
+        {
+            return "The result of the product A2.";
+        }
+    }
+}
---
+++ FactoryMethod/AbstractFactory/IAbstractProductA.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace AbstractFactory
+{
+    // Each distinct product of a product family should have a base interface.
+    // All variants of the product must implement this interface.
+    public interface IAbstractProductA
+    {
+        string UsefulFunctionA();
+    }
+}
