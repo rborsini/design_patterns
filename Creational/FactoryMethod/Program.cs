486a2d25674490f5499b4e95676d1276fc1668b6
Roberto Borsini <roberto.borsini@gmail.com>
2020-04-10 10:22:48 +0200

    aggiunto bridge

---
+++ Creational/FactoryMethod/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    /// <summary>
+    /// https://refactoring.guru/design-patterns/factory-method
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Client().Main();
+        }
+    }
+}
---
+++ Structural/Bridge/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Abstraction abstraction;
+            // The client code should be able to work with any pre-configured
+            // abstraction-implementation combination.
+            abstraction = new Abstraction(new ConcreteImplementationA());
+            var output = abstraction.Operation();
+            Console.WriteLine(output);
+
+            Console.WriteLine();
+
+            abstraction = new ExtendedAbstraction(new ConcreteImplementationB());
+            output = abstraction.Operation();
+            Console.WriteLine(output);
+
+            Console.ReadLine();
+        }
+    }
+}
